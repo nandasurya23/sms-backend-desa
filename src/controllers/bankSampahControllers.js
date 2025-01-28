b20e88031842a83b8cb6d636593341fb615b46a2
Nanda Surya <nandasurya@Mac-mini.local>
2025-01-28 16:29:45 +0800

    update logic bank_sampah

--- src/controllers/bankSampahControllers.js
+++ src/controllers/bankSampahControllers.js
@@ -21,18 +21,6 @@ const addBankSampah = async (req, res) => {
       return res.status(404).json({ error: 'User not found' });
     }
 
-    // Pastikan file gambar ada
-    // if (!req.files || req.files.length === 0) {
-    //   return res.status(400).json({ error: 'At least one image is required' });
-    // }
-
-    // Menyimpan gambar di Cloudinary
-    // const images = await Promise.all(
-    //   req.files.map(async (file) => {
-    //     const uploadResponse = await cloudinary.uploader.upload(file.path);
-    //     return uploadResponse.secure_url; // Mendapatkan URL gambar yang di-upload
-    //   })
-    // );
 
     // Menambahkan data ke tabel bank_sampah
     const { data, error } = await supabase
--- src/routes/bankSampahRoutes.js
+++ src/routes/bankSampahRoutes.js
@@ -6,6 +6,6 @@ const upload = require('../middlewares/uploadMiddleware'); // Import middleware 
 const router = express.Router();
 
 // Route untuk menambah data ke tabel bank_sampah, menggunakan upload.array untuk multiple files
-router.post('/add-bank-sampah', authenticateToken, upload.array('images[]', 5), addBankSampah); // Maksimal 5 gambar per request
+router.post('/add-bank-sampah', authenticateToken, addBankSampah); // Maksimal 5 gambar per request
 
 module.exports = router;
